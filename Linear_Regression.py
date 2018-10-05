ef863e4568cfcba6054f2a03d5fa12e565e09051
Evan Truesdale <kayakcat@truecrew.net>
2018-10-05 16:57:14 -0700

    Multivariate car data, normal curve feature scaling

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -1,21 +1,36 @@
 import numpy
+from sklearn import preprocessing
 
 def Import_Data(filename):
    training_data_filename = filename
    with open(training_data_filename, "r") as file:
-      n = len(file.readline().split(','))
+      n = len(file.readline().split("\t"))
    m = 0
    X = numpy.empty([0, n])
    Y = numpy.empty([0])
    with open(training_data_filename, "r") as file:
       for line in file:
          row = numpy.array([1])
-         temp_list = list(map(float, line.split(',')))
+         print(line.split("\t"))
+         temp_list = list(map(float, line.rstrip().split("\t")))
          Y = numpy.append(Y, temp_list[n-1:])
          row = numpy.append(row, temp_list[0:-1])
          X = numpy.append(X, [row], axis = 0)
          m += 1
-   return m, n, X, Y
+
+   mean = numpy.empty([0])
+   stdev = numpy.empty([0])
+   for i in range(0, n):
+      mean = numpy.append(mean, X[:,i].mean())
+      stdev = numpy.append(stdev, X[:,i].std())
+      if(stdev[i] != 0):
+         X[:,i] = [value - mean[i] for value in X[:,i]] / stdev[i]
+   mean = numpy.append(mean, Y.mean())
+   stdev = numpy.append(stdev, Y.std())
+   if(stdev[n] != 0):
+      Y = [value - mean[n] for value in Y] / stdev[n]
+
+   return m, n, X, Y, mean, stdev
 
 def dJ(theta, m, n, X, Y):
    steps = numpy.empty([0])
@@ -27,9 +42,9 @@ def dJ(theta, m, n, X, Y):
       steps = numpy.append(steps, sum / m)
    return steps
 
-m, n, X, Y = Import_Data("training_data.txt")
+m, n, X, Y, mean, stdev = Import_Data("training_data.txt")
 theta = numpy.zeros(n)
-alpha = 0.025
+alpha = 0.3
 print(theta)
 while True:
    last_theta = theta
@@ -38,9 +53,22 @@ while True:
    print(theta)
    stop = True
    for i in range(0, n):
-      if abs((theta[i] - last_theta[i])/theta[i]) < .000001:
+      if abs((theta[i] - last_theta[i])/theta[i]) < .0000001:
          fakeCode=stop
       else:
          stop = False
+      if(numpy.isnan(theta[i])):
+         print("Alpha is too large")
+         break
    if stop:
+      print("X")
+      print(X)
+      print("Y")
+      print(Y)
+      print("Thetas")
+      print(theta)
+      print("Means")
+      print(mean)
+      print("Stdevs")
+      print(stdev)
       break
